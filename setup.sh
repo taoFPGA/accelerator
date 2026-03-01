efa9760d14813362f7fa425a523878d60b5db41b
Eliran Turgeman <Elirantur1@gmail.com>
2026-03-01 14:50:29 +0000

    chore: add Vivado 2025.2 environment setup

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Source Vivado 2025.2 environment
+source /data/project/tsmc65/users/turgeme/xilinx/2025.2/Vivado/settings64.sh
+
+echo "Vivado 2025.2 environment loaded. Type 'vivado' to launch."
